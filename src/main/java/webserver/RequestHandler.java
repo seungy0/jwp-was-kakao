77978343f7aa85fd39c1a3a407346e2301483822
seungy0 <dltmdrb98@gmail.com>
2024-04-08 15:29:17 +0900

    feat: 요청에 맞는 Content-Type 적용
    stylesheet를 포함한 static 디렉토리 내부의 파일에 대해 적절하게 응답하도록 변경
    
    Co-authored-by: Eungu-Kwon <kiwi9084@gmail.com>

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -8,23 +8,24 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
-
 import java.nio.file.Files;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler implements Runnable {
+
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Socket connection;
+    private final Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
     }
 
     public void run() {
-        logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
+        logger.debug("New Client Connect! Connected IP : {}, Port : {}",
+            connection.getInetAddress(),
+            connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader reader = new BufferedReader(new InputStreamReader(in));
@@ -41,45 +42,28 @@ public class RequestHandler implements Runnable {
             String method = tokens[0];
             String path = tokens[1];
 
-            if (method.equals("GET")) {
-                File file = new File("./src/main/resources/templates"+path);
-                if (file.exists()) {
-                    byte[] body = Files.readAllBytes(file.toPath());
-                    DataOutputStream dos = new DataOutputStream(out);
-                    response200Header(dos, body.length);
-                    responseBody(dos, body);
-                    return;
-                }
+            File file = new File(
+                "./src/main/resources" + (path.endsWith(".html") ? "/templates" : "/static")
+                    + path);
+
+            if (!file.exists()) {
+                sendResponse(out, "404 Not Found", "text/html;charset=utf-8");
+                return;
             }
 
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "404 Not Found".getBytes();
-            response404Header(dos, body.length);
-            responseBody(dos, body);
-//            DataOutputStream dos = new DataOutputStream(out);
-//            byte[] body = "Hello World".getBytes();
-//            response200Header(dos, body.length);
-//            responseBody(dos, body);
+            byte[] body = Files.readAllBytes(file.toPath());
+            String contentType = Files.probeContentType(file.toPath());
+            sendResponse(out, body, contentType);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void response404Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 404 Not Found \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent,
+        String contentType) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Type: " + contentType + "\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
@@ -94,5 +78,21 @@ public class RequestHandler implements Runnable {
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private void sendResponse(OutputStream out, String body, String contentType)
+        throws IOException {
+        sendResponse(out, body.getBytes(), contentType);
+    }
+
+    private void sendResponse(OutputStream out, byte[] body, String contentType)
+        throws IOException {
+        DataOutputStream dos = new DataOutputStream(out);
+        dos.writeBytes("HTTP/1.1 " + (body.length == 0 ? "404 Not Found" : "200 OK") + " \r\n");
+        dos.writeBytes("Content-Type: " + contentType + "\r\n");
+        dos.writeBytes("Content-Length: " + body.length + "\r\n");
+        dos.writeBytes("\r\n");
+        dos.write(body, 0, body.length);
+        dos.flush();
     }
 }
