73d3f17d2f4280a9978608eb72914d1e89e42816
seungy0 <dltmdrb98@gmail.com>
2024-04-17 14:33:44 +0900

    refactor: 프로퍼티명 변경
    cookies -> sessions

--- src/main/java/db/SessionStore.java
+++ src/main/java/db/SessionStore.java
@@ -6,13 +6,13 @@ import java.util.Optional;
 import model.User;
 
 public class SessionStore {
-    private static final Map<String, User> cookies = new HashMap<>();
+    private static final Map<String, User> sessions = new HashMap<>();
 
     public static void addSession(String key, User user) {
-        cookies.put(key, user);
+        sessions.put(key, user);
     }
 
     public static Optional<User> getSession(String key) {
-        return Optional.ofNullable(cookies.get(key));
+        return Optional.ofNullable(sessions.get(key));
     }
 }
